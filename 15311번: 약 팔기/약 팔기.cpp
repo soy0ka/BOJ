1f70d71928f188e683a704dac571fc1f2a7bf5eb
soyoka <55011525+soy0ka@users.noreply.github.com>
2025-05-28 21:05:49 +0900

    solve: 15311

---
+++ 15311번: 약 팔기/약 팔기.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 15311                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: yurihana <boj.kr/u/yurihana>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/15311                          #+#        #+#      #+#    */
+/*   Solved: 2025/05/28 19:45:03 by yurihana      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n=0;
+  cin >> n;
+
+  cout << "2000\n";
+  for (int i=0;i<1000;i++) {
+    cout << "1 ";
+  }
+  for (int i=0;i<1000;i++) {
+    cout << "1000 ";
+  }
+}
