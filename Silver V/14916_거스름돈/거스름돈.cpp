0558010472df8d25c5c9219feec376f8eefc2aae
soyoka <55011525+soy0ka@users.noreply.github.com>
2025-11-11 10:51:59 +0900

    feat: 14916번 문제 추가 및 코드 수정

--- Silver V/14916_거스름돈/거스름돈.cpp
+++ Silver V/14916_거스름돈/거스름돈.cpp
@@ -6,11 +6,13 @@
 /*   By: yurihana <boj.kr/u/yurihana>                +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
 /*   https://boj.kr/14916                          #+#        #+#      #+#    */
-/*   Solved: 2025/09/25 15:46:14 by yurihana      ###          ###   ##.kr    */
+/*   Solved: 2025/10/21 08:47:04 by yurihana      ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 #include <numeric>
 using namespace std;
 
@@ -33,13 +35,27 @@ const int INF = 0x3f3f3f3f;
 const ll LLINF = 4'500'000'000'000'000'000LL;
 const ll MOD = 1'000'000'007LL;
 
+const vector<int> coins = { 2, 5 };
+
+int coin (vector<int>coins, int amount) {
+  vector<int> dp(amount+1, INF);
+  dp[0] = 0;
+
+  for (auto &coin : coins) {
+    for (int i=coin;i<=amount;i++) {
+      dp[i] = min(dp[i], dp[i-coin] + 1);
+    }
+  }
+  return dp[amount] != INF ? dp[amount] : -1 ;
+}
+
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.setf(ios::fixed); cout.precision(PRECISION);
 
-
- 
-
+  int n;
+  cin >> n;
+  cout << coin(coins, n);
   return 0;
 }
